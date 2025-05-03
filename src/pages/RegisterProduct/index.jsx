85f7c2b178a969a5b09485d3dfd84d44c042bf5b
salesupper <gabriel.sales@populisrh.com>
2025-05-03 15:36:50 -0300

    registro produtos

---
+++ src/pages/RegisterProduct/index.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { Container, Form } from "./styles";
+import { FiDollarSign, FiBox, FiPaperclip, FiPackage } from 'react-icons/fi';
+
+import { Link, useNavigate } from 'react-router-dom';
+
+import { api } from "../../services/api";
+
+import { Input } from "../../components/input";
+import { Button } from "../../components/Button";
+
+
+export function RegisterProduct() {
+    const [nome, setNome] = useState("")
+    const [preco, setPreco] = useState("")
+    const [quantidade, setQuantidade] = useState("")
+    const [status, setStatus] = useState("false")
+    const [imagem, setImagem] = useState("")
+
+    const navigate = useNavigate()
+    
+    function handleRegisterProduct() {
+        if (!nome || !preco || !quantidade || !imagem) {
+            return alert("Preencha todos os campos!")
+        }
+
+        api.post("/cadastro_produto", { nome, preco, quantidade, status, imagem })
+            .then(() => {
+                alert("Cadastro realizado com sucesso!")
+                navigate("/active")
+            }).catch(error => {
+                if (error.response) {
+                    alert(error.response.data.message)
+                } else {
+                    alert("Não foi possível cadastrar!")
+                }
+            })
+    }
+
+
+    return (
+        <Container>
+            <Form>
+                <h1>Web Market</h1>
+                <p>Aplicação para gerenciar os seus produtos</p>
+
+                <h2>Cadastre seu produto</h2>
+
+                <Input
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiPackage}
+                    onChange={e => setNome(e.target.value)}
+                />
+
+                <Input
+                    placeholder="Preço"
+                    type="number"
+                    step="0.01"
+                    min="0"
+                    icon={FiDollarSign}
+                    onChange={e => setPreco(e.target.value)}
+                />
+
+                <Input
+                    placeholder="Quantidade"
+                    type="number"
+                    min="1"
+                    icon={FiBox}
+                    onChange={e => setQuantidade(e.target.value)}
+                />
+
+                <Input
+                    placeholder="Imagem"
+                    type="file"
+                    accept="image/*"
+                    icon={FiPaperclip}
+                    onChange={e => setImagem(e.target.files[0])}
+                />
+
+
+                <Button title="Cadastrar" onClick={handleRegisterProduct} />
+
+
+            </Form>
+        </Container>
+    )
+}
--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { SignIn } from '../pages/SignIn';
 import { SignUp } from '../pages/SignUp';
 import { Active } from '../pages/Active';
+import { RegisterProduct } from '../pages/RegisterProduct'; 
 
 export function AuthRoutes() {
   const user = localStorage.getItem('@web-market:user')
@@ -12,6 +13,7 @@ export function AuthRoutes() {
       <Route path="/" element={<SignIn />}/>
       <Route path="/register" element={<SignUp />}/>
       <Route path="/active" element={<Active />}/>
+      <Route path="/register_product" element={<RegisterProduct />}/>
 
       {!user && <Route path="*" element={<Navigate to="/"/>}/> }
     </Routes>
